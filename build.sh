31c0e1fc02a66595196fbcbde978eb2c87136744
e_112 <joseph271387@gmail.com>
2026-07-03 20:51:43 +0800

    update build.sh
    idk if this will work. im just testing it

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install uv && uv pip install -r uv.lock
+pip install uv && uv sync --frozen
 
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C .
